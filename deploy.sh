b3b67c3f375312ee568ee18110648c84936d47e0
jtango0710@gmail.com <jtango0710@gmail.com>
2021-09-25 15:00:44 +0800

    fix script

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,5 @@ docker push jntango/multi-worker:$SHA
 
 kubectl apply -f ./k8s
 kubectl set image deployments/server-deployment server=jntango/multi-server:$SHA
-kubectl set image deployments/client-deployment server=jntango/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=jntango/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=jntango/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jntango/multi-worker:$SHA
